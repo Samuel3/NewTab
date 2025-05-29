5070060c4260a61701e627bbc0aa3d1a2f31a137
Samuel Mathes <samuel.mathes@gmx.de>
2025-05-29 11:20:30 +0200

    Implemented bookmarks tile

--- apps/new-page/src/app/components/bookmark-tile/bookmark-tiles.component.ts
+++ apps/new-page/src/app/components/bookmark-tile/bookmark-tiles.component.ts
@@ -1,20 +1,64 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+interface Bookmark {
+  id: string;
+  title: string;
+  url: string;
+  favicon?: string;
+}
 
 @Component({
   selector: 'new-page-bookmark-tile',
   templateUrl: './bookmark-tiles.component.html',
   styleUrls: ['./bookmark-tiles.component.scss'],
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
 })
-export class BookmarkTilesComponent {
+export class BookmarkTilesComponent implements OnInit {
   @Input() name = '';
   @Input() editMode = false;
+  bookmarks: Bookmark[] = [];
 
-  constructor() {
-    chrome.bookmarks.getTree((bookmarks) => {
-      console.log('Bookmarks:', bookmarks);
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.loadBookmarks();
+  }
+
+  private loadBookmarks() {
+    chrome.bookmarks.getTree((bookmarkTreeNodes) => {
+      console.log('Received bookmarks:', bookmarkTreeNodes);
+      this.bookmarks = [];
+      this.processBookmarkNodes(bookmarkTreeNodes);
+      this.cdr.detectChanges();
+    });
+  }
+
+  private getFaviconUrl(url: string): string {
+    try {
+      const urlObj = new URL(url);
+      return `https://www.google.com/s2/favicons?domain=${urlObj.hostname}&sz=64`;
+    } catch (e) {
+      return 'assets/default-favicon.png';
+    }
+  }
+
+  private processBookmarkNodes(nodes: chrome.bookmarks.BookmarkTreeNode[]) {
+    nodes.forEach(node => {
+      if (node.url) {
+        const bookmark: Bookmark = {
+          id: node.id,
+          title: node.title || 'Unbenannt',
+          url: node.url,
+          favicon: this.getFaviconUrl(node.url)
+        };
+        console.log('Adding bookmark:', bookmark);
+        this.bookmarks.push(bookmark);
+      }
+      if (node.children) {
+        this.processBookmarkNodes(node.children);
+      }
     });
   }
 }
